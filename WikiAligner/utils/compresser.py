d0452bcce4b8b606cff9b4774a4f7f4cbeadbed9
Minghao2812 <Minghao2812@users.noreply.github.com>
2022-04-06 19:13:15 +0800

    Create class "Compresser" for big model pre-compression

--- WikiAligner/utils/compresser.py
+++ WikiAligner/utils/compresser.py
@@ -1,10 +1,25 @@
+"""Compress big models in advance. Don't have to run online."""
+
 from sentence_transformers import SentenceTransformer
 import pickle
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR: WikiTrans base directory.
 
 
 class Compresser:
-    def compress_labse(self, save_path: str = './labse_model.pkl'):
+    def __init__(self) -> None:
+        self.model_path = os.path.join(BASE_DIR, 'models')
+
+    def compress_labse(self):
         model = SentenceTransformer('sentence-transformers/LaBSE')
-        with open(save_path, 'wb') as f:
+        labse_path = os.path.join(self.model_path, 'labse_model.pkl')
+        with open(labse_path, 'wb') as f:
             pickle.dump(model, f)
-        print(f'LaBSE model have been compressed at {save_path}')
+        print(f'LaBSE model have been compressed at {labse_path}')
+
+
+if __name__ == '__main__':
+    compresser = Compresser()
+    compresser.compress_labse()
