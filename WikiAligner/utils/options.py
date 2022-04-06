607fd5e0fccd8fbf5e79ad5eb3a052e14f7842b8
Minghao2812 <Minghao2812@users.noreply.github.com>
2022-04-06 19:19:55 +0800

    Update options.py

--- WikiAligner/utils/options.py
+++ WikiAligner/utils/options.py
@@ -1,22 +1,10 @@
 class Options:
-    def print_options(self, options, prompt):
-        """Print options as (<index>)......<option>
-        """
-        if prompt:
-            print(prompt)
+    def __init__(self, name, command) -> None:
+        self.name = name
+        self.command = command
 
-        for idx, option in enumerate(options):
-            # If <option> itself has several sub-options, separate them with spaces.
-            if isinstance(option, list) or isinstance(option, tuple):
-                option_str = '    '.join(option)
-                print(f'({idx + 1})......{option_str}')
-            print(f'({idx + 1})......{option}')
+    def choose(self):
+        self.command()
 
-    def get_option_choice(self, options, prompt: str = ''):
-        """Get option choice from user input. <option index> = <user input> - 1.
-        """
-        if prompt:
-            print(prompt)
-
-        option_idx = int(input()) - 1
-        return options[option_idx]
+    def __str__(self) -> str:
+        return self.name
