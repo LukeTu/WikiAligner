053f1e078d39dcd23132c640d6892ad47cb0e4f6
Minghao2812 <Minghao2812@users.noreply.github.com>
2022-04-27 21:22:20 +0800

    Add path-check

--- WikiAligner/utils/download_manager.py
+++ WikiAligner/utils/download_manager.py
@@ -11,6 +11,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 class DownloadManager:
     def __init__(self, debug_mode=False) -> None:
         self.data_path = os.path.join(BASE_DIR, 'data')
+
+        if not os.path.exists(self.data_path):
+            os.makedirs(self.data_path)
+
         self.debug_mode = debug_mode
 
     def get_query(self, prompt):
@@ -75,7 +79,7 @@ class DownloadManager:
 
     def download_text(self,
                       title: str = '',
-                      languageCode: str = 'en') -> 'str':
+                      language_code: str = 'en') -> 'str':
         """Download the wiki content of the target title and langcode.
         return: str, the content(raw text).
         For more on the api @https://www.mediawiki.org/w/api.php?action=help&modules=main
@@ -85,7 +89,7 @@ class DownloadManager:
         #TODO: where is simplified Chinese? 可能 wiki_title是史蒂夫·賈伯斯，而非史蒂夫·乔布斯。两者url其余部分一致。
         #并不是，本身是简体的。zh ⋅⋅⋅⋅ 史蒂夫·乔布斯 ⋅⋅⋅⋅ https://zh.wikipedia.org/wiki/%E5%8F%B2%E8%92%82%E5%A4%AB%C2%B7%E4%B9%94%E5%B8%83%E6%96%AF
         # 可能是api的问题？api是否会默认设置地区？
-        url = f'https://{languageCode}.wikipedia.org/w/api.php'
+        url = f'https://{language_code}.wikipedia.org/w/api.php'
         params = {
             'action': 'query',
             'prop': 'extracts',
@@ -99,10 +103,8 @@ class DownloadManager:
         jsonText = requests.Session().get(url=url, params=params).json()
         return jsonText['query']['pages'][0]['extract']
 
-    def save_text(self, text: str, wiki_title: str,
-                  language_code: str) -> None:
-        filepath = os.path.join(self.data_path,
-                                f'{wiki_title}_{language_code}.txt')
+    def save_text(self, text: str, title: str, language_code: str) -> None:
+        filepath = os.path.join(self.data_path, f'{title}_{language_code}.txt')
         with open(filepath, 'w', encoding='utf-8') as f:
             f.writelines(text)
         print(f'Text saved at {filepath}')
