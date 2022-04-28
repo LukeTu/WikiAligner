a6756c185cad97565d25970fa3ef8db658484cdd
Minghao2812 <Minghao2812@users.noreply.github.com>
2022-04-28 16:17:39 +0800

    Optimize model-load and encoding logic
    Only load the model once and encode twice.

--- WikiAligner/utils/embedder.py
+++ WikiAligner/utils/embedder.py
@@ -9,28 +9,35 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 class Embedder:
     def __init__(self) -> None:
         self.model_path = os.path.join(BASE_DIR, 'models')
+        if not os.path.exists(self.model_path):
+            raise ValueError(f'Invalid model path at \n{self.model_path}')
 
-    # def embed_auto(self, method='labse'):
-    #     if method == 'labse':
-    #         self.load_labse()
-    #         self.embed_with_labse()
+    def embed_auto(self,
+                   text1: 'list[str]',
+                   text2: 'list[str]',
+                   method: 'str' = 'labse'):
+        if method == 'labse':
+            model = self.load_labse()
+            embedding1 = model.encode(text1)
+            embedding2 = model.encode(text2)
+            return embedding1, embedding2
 
-    # def load_labse(self):
-    #     labse_path = os.path.join(self.model_path, 'labse_model.pkl')
-    #     print('Loading LaBSE model...')
-    #     with open(labse_path, 'rb') as f:
-    #         labse_model = pickle.load(f)
-    #     print('LaBSE model has been loaded!')
-    #     return labse_model
-
-    def embed_with_labse(self, text, labse_model):
+    def load_labse(self):
         labse_path = os.path.join(self.model_path, 'labse_model.pkl')
         print('Loading LaBSE model...')
         with open(labse_path, 'rb') as f:
             labse_model = pickle.load(f)
         print('LaBSE model has been loaded!')
-        print('Creating embedding with LaBSE...')
-        return labse_model.encode(text)
+        return labse_model
 
     def embed_with_doc2vec(self, text):
         pass
+
+    # def embed_with_labse(self, text):  #, labse_model
+    #     labse_path = os.path.join(self.model_path, 'labse_model.pkl')
+    #     print('Loading LaBSE model...')
+    #     with open(labse_path, 'rb') as f:
+    #         labse_model = pickle.load(f)
+    #     print('LaBSE model has been loaded!')
+    #     print('Creating embedding with LaBSE...')
+    #     return labse_model.encode(text, show_progress_bar=True)
